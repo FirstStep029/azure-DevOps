4c429d770e6d3ac47d5a361bc8fb8ffa3a8ca3d2
First Step <FirstStep029@gmail.com>
2023-02-21 23:40:55 +0530

    Update drop-blob-storage.ps1

--- blob-storage/scripts/drop-blob-storage.ps1
+++ blob-storage/scripts/drop-blob-storage.ps1
@@ -6,8 +6,8 @@ $name_exists = az storage account check-name --name $blob_name | ConvertFrom-Jso
 Write-Host $name_exists
 
 if($name_exists.nameAvailable) {
-    Write-Host "'$($blob_name)' Available & Dropping."
-    az storage account delete --name $blob_name --resource-group $env:RESOURCE_GROUP
-} else {
     Write-Host "'$($blob_name)' Does Not Exists, Skipping."
+} else {
+    Write-Host "'$($blob_name)' Available & Dropping."
+    az storage account delete --name $blob_name --resource-group $env:RESOURCE_GROUP    
 }
