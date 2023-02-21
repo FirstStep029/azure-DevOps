9a915ac79b1221fb7fc0dcc69d3ed630469736dd
First Step <FirstStep029@gmail.com>
2023-02-21 22:27:45 +0530

    Update create-blob-storage.ps1

--- blob-storage/scripts/create-blob-storage.ps1
+++ blob-storage/scripts/create-blob-storage.ps1
@@ -1,1 +1,15 @@
-Write-Host "BLOB-NAME: $($env:NAME) `nSKU: $($env:SKU) `nRG: $($env:RESOURCE_GROUP) `nLOCATION: $($env:LOCATION) `nACCESS_TIER: $($env:ACCESS_TIER)"
+# Write-Host "BLOB-NAME: $($env:NAME) `nSKU: $($env:SKU) `nRG: $($env:RESOURCE_GROUP) `nLOCATION: $($env:LOCATION) `nACCESS_TIER: $($env:ACCESS_TIER)"
+
+
+# Check Storage Name is Valid
+$name_exists = az storage account check-name --name $env:NAME.ToLower() | ConvertFrom-Json
+
+if($name_exists.nameAvailable) {
+    Write-Host "Name '$($blob_prop.'name')' Available."
+} else {
+    Write-Host "Name '$($blob_prop.'name')' Already Taken."
+    exit 1
+}
+
+# Create Blob Storage Account
+# az storage account create --name $blob_prop.'name' --resource-group $blob_prop.'resource-group' --location $blob_prop.'region'
