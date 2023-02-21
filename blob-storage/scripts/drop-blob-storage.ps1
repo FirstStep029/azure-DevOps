300c5cf7e4a26a01e2506ccab8f4ea10589e556d
First Step <FirstStep029@gmail.com>
2023-02-21 23:29:42 +0530

    update

---
+++ blob-storage/scripts/drop-blob-storage.ps1
@@ -0,0 +1,12 @@
+# create name with remove Special Characters and convert to lower case
+$blob_name = $env:NAME.ToLower() -replace '\W',''
+
+# Check Storage Name is Valid
+$name_exists = az storage account check-name --name $blob_name | ConvertFrom-Json
+
+if($name_exists.nameAvailable) {
+    Write-Host "'$($blob_name)' Available & Dropping."
+    az storage account delete --name $blob_name --resource-group $env:RESOURCE_GROUP
+} else {
+    Write-Host "'$($blob_name)' Does Not Exists, Skipping."
+}
